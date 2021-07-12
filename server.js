1d2a7732f3a06fd0140e3b5250907345abb057e9
nowann1 <79383025+nowann1@users.noreply.github.com>
2021-07-12 05:27:09 -0300

    Minor fix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
-const port = 5000;
+const PORT = 5500;
 const appRouter = require("./routes/swapi_routes");
 
 app.use(logger("dev"));
 
 app.use("/", appRouter);
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
